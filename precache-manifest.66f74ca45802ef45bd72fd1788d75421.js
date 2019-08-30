7aeccfc381c0131a7863d49889ea2bfba70714be
Akshay Joy <akshayjoyinfo@gmail.com>
2019-08-30 10:47:56 +0530

    Updates

---
+++ precache-manifest.66f74ca45802ef45bd72fd1788d75421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f59c42a5f23c709509a548156a7415b",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "c438c2d0af32e9763bf1",
+    "url": "/me/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "eb80fda5dbd4195c7034",
+    "url": "/me/static/js/2.fbf32552.chunk.js"
+  },
+  {
+    "revision": "c438c2d0af32e9763bf1",
+    "url": "/me/static/js/main.e3b67c4b.chunk.js"
+  },
+  {
+    "revision": "b16b36f6102d0cc8f957",
+    "url": "/me/static/js/runtime~main.d6be6db6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/me/static/media/logo.5d5d9eef.svg"
+  }
+]);
